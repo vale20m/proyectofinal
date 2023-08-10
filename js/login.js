5599a83af5ba9a715453300c9e3be7008ba18523
Vale20m <valentinveintemilla@gmail.com>
2023-08-09 23:08:41 -0300

    Agregamos mejoras al menú y funcionamiento del login

--- js/login.js
+++ js/login.js
@@ -2,8 +2,24 @@
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const boton = document.getElementById("boton");
+const enviar = document.getElementById("enviar");
 
-boton.addEventListener("click", function(){
-    window.location.href="index.html";
+enviar.addEventListener("click", function(){
+    if (email.value != "" && password.value != ""){
+        window.location.href="index.html";
+    }
+});
+
+// Función para mostrar y ocultar contraseña
+
+const botonMostrar = document.querySelector("#mostrar");
+
+botonMostrar.addEventListener("change", function(){
+    if (botonMostrar.checked){
+        password.type = "text";
+        mostrar = true;
+    } else {
+        password.type = "password";
+        mostrar = false;
+    }
 });

576ea72f17ae3c283bcb12f1345cad9493aea0e2
Vale20m <valentinveintemilla@gmail.com>
2023-08-10 21:01:19 -0300

    Cambios en la función login del usuario

--- js/login.js
+++ js/login.js
@@ -1,15 +1,3 @@
-// Función para redirigir al usuario a la página principal luego de iniciar sesión
-
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const enviar = document.getElementById("enviar");
-
-enviar.addEventListener("click", function(){
-    if (email.value != "" && password.value != ""){
-        window.location.href="index.html";
-    }
-});
-
 // Función para mostrar y ocultar contraseña
 
 const botonMostrar = document.querySelector("#mostrar");

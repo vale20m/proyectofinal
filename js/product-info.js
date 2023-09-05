f6aeb137cc2d10efe9c2f5e68b3cb81cfeb1348c
Vale20m <valentinveintemilla@gmail.com>
2023-09-04 23:31:58 -0300

    Agregamos los ultimos detalles de la funcionalidad 2

--- js/product-info.js
+++ js/product-info.js
@@ -9,17 +9,18 @@ const CAT_PRODUCTOS = "https://japceibal.github.io/emercado-api/products/" + Pro
 
 const container = document.getElementById("infoProduct");
 
-
-
 function showData(product) {
   // El for itera sobre las imágenes del producto
-  container.innerHTML += `<h1 class="shadow p-3 mb-3 mt-3 bg-body rounded">${product.name}</h1> <h1 class="shadow p-3 mb-3 bg-body rounded">Costo: ${product.currency} ${product.cost}</h1> <h1 class="shadow p-3 mb-3 bg-body rounded">Vendidos: ${product.soldCount} </h1>
-  <p class="fs-2 shadow p-3 mb-5 bg-body rounded">${product.description}<p> <br>`;
-  container.innerHTML += `<div class="d-flex justify-content-around">`;
+  container.innerHTML += `<div class="container"><h1 class="px-5 my-4 text-uppercase">${product.name}</h1> <hr>
+  <p class="fs-3 shadow p-3 mb-3 mt-4 bg-body rounded fst-italic">${product.description}<p>
+  <h2 class="shadow p-3 my-3 bg-body rounded">Precio: ${product.currency} ${product.cost}</h2>
+  <h2 class="shadow p-3 mb-5 bg-body rounded">Ventas: ${product.soldCount} </h2>
+  <p class="fs-4">Imágenes meramente ilustrativas: </p> <br>`;
+  container.innerHTML += `<div class="container">`;
   for (let i = 0; i < product.images.length; i++){
-    container.innerHTML += `<img class="img-thumbnail" src="${product.images[i]}">`;
+    container.innerHTML += `<img class="w-50 border border-5 border-white shadow-sm p-3 mb-1 bg-body rounded" src="${product.images[i]}">`;
   }
-  container.innerHTML += `</div>`;
+  container.innerHTML += `</div></div>`;
 }
 
 

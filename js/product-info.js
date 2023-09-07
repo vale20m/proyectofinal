6c4e5d26bf5612060b7e0ee8fa09d4a8f6a477b1
Vale20m <valentinveintemilla@gmail.com>
2023-09-07 20:01:46 -0300

    Pequeños cambios

--- js/product-info.js
+++ js/product-info.js
@@ -10,13 +10,13 @@ const CAT_PRODUCTOS = "https://japceibal.github.io/emercado-api/products/" + Pro
 const container = document.getElementById("infoProduct");
 
 function showData(product) {
-  // El for itera sobre las imágenes del producto
   container.innerHTML += `<div class="container"><h1 class="px-5 my-4 text-uppercase">${product.name}</h1> <hr>
   <p class="fs-3 shadow p-3 mb-3 mt-4 bg-body rounded fst-italic">${product.description}<p>
   <h2 class="shadow p-3 my-3 bg-body rounded">Precio: ${product.currency} ${product.cost}</h2>
   <h2 class="shadow p-3 mb-5 bg-body rounded">Ventas: ${product.soldCount} </h2>
   <p class="fs-4">Imágenes meramente ilustrativas: </p> <br>`;
   container.innerHTML += `<div class="container">`;
+  // El for itera sobre las imágenes del producto
   for (let i = 0; i < product.images.length; i++){
     container.innerHTML += `<img class="w-50 border border-5 border-white shadow-sm p-3 mb-1 bg-body rounded" src="${product.images[i]}">`;
   }

9ccf84a7eecb02d302f80548ca6cbc8da025ae9b
Valentín Veintemilla <valentinveintemilla@gmail.com>
2023-10-08 14:40:23 -0300

    Cambios para hacer el carrito responsive

--- js/cart.js
+++ js/cart.js
@@ -20,35 +20,38 @@ function calculateSubtotal(string, num1, num2){
 function showCart(array){
 
     // SECCIÓN QUE FUNCIONA COMO "TÍTULO" DE LA TABLA DEL CARRITO
-    cartTitle.innerHTML = 
-    `<tr>
-        <th scope="col"></th>
-        <th scope="col">Nombre</th>
-        <th scope="col">Costo</th>
-        <th scope="col">Cantidad</th>
-        <th scope="col">Subtotal</th>
-    </tr>`
+    cartItems.innerHTML += 
+    `<div class="row">
+        <div class="col-md-3 d-md-block d-none"></div>
+        <div class="col-md-3 d-md-block d-none">Nombre</div>
+        <div class="col-md-2 d-md-block d-none">Costo</div>
+        <div class="col-md-2 d-md-block d-none">Cantidad</div>
+        <div class="col-md-2 d-md-block d-none">Subtotal</div>
+    </div>`
     for (const item of array) {
 
         // CREAMOS UNA SECCION DE LA TABLA DONDE AGREGAREMOS LOS ELEMENTOS EXTRAIDOS DEL JSON Y DEL LOCAL STORAGE
 
-        const row = document.createElement("tr");
+        const row = document.createElement("div");
         row.innerHTML =
-        `<td id="cartItemImage"><img class="img-thumbnail" src="${item.image}"></th>
-        <td>${item.name}</td>
-        <td>${item.currency} ${item.unitCost}</td>
-        <td class="w-25"><input type="number" class="w-100 form-control" value="${item.count}" min="1"></td>
-        <td id="subtotal">${calculateSubtotal(item.currency, item.unitCost, item.count)}</td>`;
-
+        `<div class="list-group-item border rounded">
+          <div class="row mx-auto">
+            <div class="col-md-2 col-sm-4 offset-sm-0 col-5 offset-1 my-auto"><img id="cartItemImage" class="img-thumbnail my-auto" src="${item.image}"></div>
+            <h4 class="col-lg-2 col-md-3 col-sm-4 col-6 my-auto mx-auto">${item.name}</h4>
+            <h4 class="col-md-2 col-sm-4 d-sm-block d-none my-auto">${item.currency} ${item.unitCost}</h4>
+            <h4 class="d-md-none col-sm-3 offset-sm-0 col-5 offset-1 my-md-auto mt-sm-3 mt-3">Cantidad: </h4><div class="col-md-2 col-sm-3 col-6 my-md-auto mt-3 mt-2"><input type="number" class="form-control w-75" value="${item.count}" min="1"></div>
+            <h4 class="d-md-none col-sm-3 offset-sm-0 col-5 offset-1 my-md-auto mt-sm-3 mt-2">Subtotal: </h4><h4 id="subtotal" class="col-md-2 col-sm-3 col-6 my-md-auto mt-2">${calculateSubtotal(item.currency, item.unitCost, item.count)}</h4>
+          </div>
+        </div>`;
         // ANIDAMOS UN ADD EVENT LISTENER AL ELEMENTO "INPUT", EL CUAL EJECUTA UNA FUNCION
         // QUE MODIFICA EL SUBTOTAL DEL PRODUCTO CADA VEZ QUE SE MODIFICA EL VALOR DE DICHO INPUT.
 
         const inputCount = row.querySelector("input");
         inputCount.addEventListener("input", function(){
 
-            const newCount = inputCount.value;
-            const subtotalContainer = row.querySelector("#subtotal");
-            subtotalContainer.textContent = calculateSubtotal(item.currency, item.unitCost, newCount);
+          const newCount = inputCount.value;
+          const subtotalContainer = row.querySelector("#subtotal");
+          subtotalContainer.textContent = calculateSubtotal(item.currency, item.unitCost, newCount);
         
         });
 
@@ -58,7 +61,7 @@ function showCart(array){
     }
 
     cartControls.innerHTML =
-    `<div class="col-lg-6 col-md-8 col-10 mx-5">
+    `<div class="col-lg-6 col-md-8 col-10 mx-sm-5 mx-4">
     <h2>Tipo de envío</h2>
     <form id="checkoutForm">
       <div class="mb-3">

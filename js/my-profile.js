536955ebdb4f9545251954cf3e9a5d5f4ff6edb8
Valentín Veintemilla <valentinveintemilla@gmail.com>
2023-10-30 17:59:13 -0300

    Algunos cambios en el codigo

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,97 +1,123 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el correo electrónico del `localStorage`.
-    const Emailtemp = localStorage.getItem("email");
-  
-    // Verificar si el usuario está logueado.
-    if (Emailtemp) {
-      // Rellenar automáticamente el campo de correo con el valor del `localStorage`.
-      const emailInput = document.getElementById("email");
-      emailInput.value = Emailtemp;
-  
-      // Cargar los datos del perfil si existen en el `localStorage`.
-      const storedProfileData = localStorage.getItem(Emailtemp);
-      if (storedProfileData) {
-        const userProfile = JSON.parse(storedProfileData);
-        // Rellenar los campos del perfil con los datos recuperados.
-        const nombreInput = document.getElementById("nombre");
-        const segundoNombreInput = document.getElementById("segundo-nombre");
-        const apellidoInput = document.getElementById("apellido");
-        const segundoApellidoInput = document.getElementById("segundo-apellido");
-        const telefonoInput = document.getElementById("telefono");
-  
-        nombreInput.value = userProfile.nombre;
-        segundoNombreInput.value = userProfile.segundoNombre;
-        apellidoInput.value = userProfile.apellido;
-        segundoApellidoInput.value = userProfile.segundoApellido;
-        telefonoInput.value = userProfile.telefono;
-  
-        // Cargar la imagen de perfil si existe.
-        if (userProfile.profileImage) {
-          const perfilImagedefecto = document.getElementById("imagen-Muestra");
-          perfilImagedefecto.src = userProfile.profileImage;
-        }
+    
+  // Obtenemos el correo electrónico del `localStorage`
+
+  const emailTemp = localStorage.getItem("email");
+
+  // Guardamos en constantes los elementos del HTML
+
+  const emailInput = document.getElementById("email");
+  const nameInput = document.getElementById("name");
+  const secondNameInput = document.getElementById("secondName");
+  const lastNameInput = document.getElementById("lastName");
+  const secondLastNameInput = document.getElementById("secondLastName");
+  const phoneInput = document.getElementById("phone");
+
+  // Verificamos que el usuario este logueado
+
+  if (emailTemp) {
+
+    // Rellena automáticamente el campo de correo con el valor del `localStorage` (el correo actual).
+
+    emailInput.value = emailTemp;
+
+    // En caso de que el usuario ya los haya llenado e ingrese nuevamente, se cargan los datos anteriores
+
+    const userProfile = JSON.parse(localStorage.getItem(emailTemp));
+
+    if (userProfile) {
+
+      // Se rellenan los campos
+
+      nameInput.value = userProfile.name;
+      secondNameInput.value = userProfile.secondName;
+      lastNameInput.value = userProfile.lastName;
+      secondLastNameInput.value = userProfile.secondLastName;
+      phoneInput.value = userProfile.phone;
+
+      // Cargamos la imagen de perfil si existe.
+
+      if (userProfile.profileImage) {
+        const defaultProfileImage = document.getElementById("shownPicture");
+        defaultProfileImage.src = userProfile.profileImage;
       }
     }
-    
-    // Obtener el formulario del perfil.
-    const perfilForm = document.getElementById("perfil-form");
+  }
   
-    perfilForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-  
-      const emailInput = document.getElementById("email");
-      const userEmail = emailInput.value;
-  
-      if (userEmail) {
-        // Verificar si el correo en el `localStorage` coincide con el correo ingresado.
-        const Emailtemp = localStorage.getItem("email");
-  
-        if (Emailtemp === userEmail) {
-          // Los correos coinciden, guardar los datos del perfil en el `localStorage`.
-          const nombreInput = document.getElementById("nombre");
-          const segundoNombreInput = document.getElementById("segundo-nombre");
-          const apellidoInput = document.getElementById("apellido");
-          const segundoApellidoInput = document.getElementById("segundo-apellido");
-          const telefonoInput = document.getElementById("telefono");
-  
-          if (nombreInput.value && apellidoInput.value) {
-            const userProfile = {
-              nombre: nombreInput.value,
-              segundoNombre: segundoNombreInput.value,
-              apellido: apellidoInput.value,
-              segundoApellido: segundoApellidoInput.value,
-              telefono: telefonoInput.value,
-              // Convertir la imagen a base64 y guardarla.
-              profileImage: document.getElementById("imagen-Muestra").src,
-            };
-  
-            // Guardar los datos del perfil en el `localStorage` utilizando el correo como clave.
-            localStorage.setItem(userEmail, JSON.stringify(userProfile));
-  
-            alert("Datos del perfil actualizados exitosamente.");
-          } else {
-            alert("Por favor, complete los campos obligatorios.");
-          }
+  // Obtener el formulario del perfil
+
+  const profileForm = document.getElementById("perfil-form");
+
+  profileForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const userEmail = emailInput.value;
+
+    if (userEmail) {
+
+      if (emailTemp === userEmail) {
+
+        // Si el correo de la cuenta actual coincide con el input, guardar los cambios en el `localStorage`
+
+        if (nameInput.value && lastNameInput.value) {
+          const userProfile = {
+            name: nameInput.value,
+            secondName: secondNameInput.value,
+            lastName: lastNameInput.value,
+            secondLastName: secondLastNameInput.value,
+            phone: phoneInput.value,
+            
+            // Traemos la imagen del HTML y la guardamos
+
+            profileImage: document.getElementById("shownPicture").src
+          };
+
+          // Guardar los datos del perfil en el `localStorage` utilizando el correo como clave
+
+          localStorage.setItem(userEmail, JSON.stringify(userProfile));
+
+          // Le damos un feedback al usuario sobre la situación
+
+          alert("Datos del perfil actualizados exitosamente.");
+
         } else {
-          alert("El correo ingresado no coincide con el correo almacenado. Debe registrarse nuevamente.");
+
+          alert("Por favor, complete los campos obligatorios.");
+
         }
+
       } else {
-        alert("Por favor, complete el campo de correo.");
+
+        alert("El correo ingresado no coincide con el correo almacenado. Debe registrarse nuevamente.");
+      
       }
-    });
-  
-    // Obtener el campo de selección de imagen.
-    const profileImageInput = document.getElementById("perfil-image");
-  
-    profileImageInput.addEventListener("change", function (e) {
-      const selectedImage = e.target.files[0];
-      if (selectedImage) {
-        const reader = new FileReader();
-        reader.onload = function (event) {
-          const perfilImagedefecto = document.getElementById("imagen-Muestra");
-          perfilImagedefecto.src = event.target.result;
-        };
-        reader.readAsDataURL(selectedImage);
-      }
-    });
+
+    } else {
+
+      alert("Por favor, complete el campo de correo.");
+
+    }
   });
+
+  // Obtenemos el campo de selección de imagen
+
+  const profileImageInput = document.getElementById("profileImage");
+
+  profileImageInput.addEventListener("change", function (e) {
+    
+    const selectedImage = e.target.files[0];
+
+    if (selectedImage) {
+
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const defaultProfileImage = document.getElementById("shownPicture");
+        defaultProfileImage.src = event.target.result;
+      };
+      reader.readAsDataURL(selectedImage);
+
+    }
+
+  });
+
+});
